3db11c22f71fdcba92fb64cafb5d4842f8a9050d
Kiran Junnarkar <kjunnarkar@yahoo.com>
2019-12-19 19:52:49 -0800

    Updated App.js/Login/NewUser

--- muscle-pro/src/App.js
+++ muscle-pro/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Route, Link } from 'react-router-dom';
-
+import { Route } from 'react-router-dom';
+import Login from './components/Login';
+import NewUser from './components/NewUser';
 import './App.css';
 
 function App() {
@@ -9,6 +10,8 @@ function App() {
       <header className="App-header">
         <h1>Muscle Pro</h1>
       </header>
+      <Route exact path='/' component={Login} />
+      <Route path='/newuser' component={NewUser} />
     </div>
   );
 }
