894fcea7b7397f791b88ad86e1fb07919ba51e30
Toniddarden1 <Toniddarden@gmail.com>
2019-12-19 23:20:12 -0500

    scaffolded

---
+++ muscle-pro/src/store/reducers/index.js
@@ -0,0 +1,9 @@
+
+
+// I thought maybe it would be easier to combine them so we could work someone indpendently but 
+// let me know either or works for me -TD
+
+export default combineReducers({
+    
+    }
+);
