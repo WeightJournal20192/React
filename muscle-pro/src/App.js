ff483dbc238b0feb544251969a9c1e43a11e7d1c
Daniel Hermansen <daniel.j.hermansen@gmail.com>
2019-12-19 20:49:14 -0700

    organizing App.js nav bar. Starting to build the component for the WelcomePage.JS

--- muscle-pro/src/App.js
+++ muscle-pro/src/App.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import { Route, Link } from 'react-router-dom';
-
+import WelcomePage from "./components/WelcomePage";
 import './App.css';
 
 function App() {
+
   return (
     <div className="App">
+      <nav>
+        <h1 className="App-name">Muscle Pro</h1>
+        <div className="Nav-links">
+          <h2>Exercises</h2>
+          <h2>Profile</h2> 
+          {/* this would include goals, measurments, progress pics? */}
+        </div>
+      </nav>
       <header className="App-header">
-        <h1>Muscle Pro</h1>
+
       </header>
     </div>
   );
---
+++ muscle-pro/src/components/WelcomePage.js
@@ -0,0 +1,9 @@
+import React, { useState } from "react";
+import { Route, Link } from "react-router-dom";
+
+function WelcomePage(props) {
+
+    return ;
+}
+
+export default WelcomePage;
