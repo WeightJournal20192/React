0526952665f88d4b5ee279edf7e7d479c75bc553
jr <jrdugue@gmail.com>
2020-01-07 22:58:48 -0500

    latest

--- muscle-pro/src/App.js
+++ muscle-pro/src/App.js
@@ -5,9 +5,9 @@ import NewUser from './components/NewUser';
 import WelcomePage from './components/WelcomePage';
 import Exercises from './components/Exercises';
 import './App.css';
-// import {createStore} from "redux";
+import {createStore} from "redux";
 // import {FamilyProvider} from "./components/FamilyContext";
-import PrivateRoute from "./components/PrivateRoute";
+import PrivateRoute from "./utils/PrivateRoute";
 
 function App() {
   return (
@@ -16,7 +16,7 @@ function App() {
         <header className="App-header">
           <h1>Muscle Pro</h1>
         </header>
-        <Route exact path='/login' component={Login} />
+        <Route exact path='/' component={Login} />
         <Route path='/newuser' component={NewUser} />
         <PrivateRoute exact path='/WelcomePage' component={WelcomePage} />
         <PrivateRoute exact path='/Exercises' component={Exercises} />
