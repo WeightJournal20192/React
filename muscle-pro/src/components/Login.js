a2eb86ce679cf21748a596812e7225e66bb0dd24
Toniddarden1 <Toniddarden@gmail.com>
2020-01-09 19:26:35 -0500

    pulling jd -b

--- muscle-pro/src/components/Login.js
+++ muscle-pro/src/components/Login.js
@@ -1,114 +1,114 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import { useForm } from 'react-hook-form';
-import WelcomePage from './WelcomePage';
-import styled from 'styled-components';
-import axios from 'axios';
+// import React, { useState } from 'react';
+// import { Link } from 'react-router-dom';
+// import { useForm } from 'react-hook-form';
+// import WelcomePage from './WelcomePage';
+// import styled from 'styled-components';
+// import axios from 'axios';
 
-// styled-components
-const FormHeading = styled.h2 `
-    margin-top: 40px;
-    margin-bottom: 20px;
-`;
+// // styled-components
+// const FormHeading = styled.h2 `
+//     margin-top: 40px;
+//     margin-bottom: 20px;
+// `;
 
-const FormSetup = styled.form `
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    margin-top: 40px;
-`;
+// const FormSetup = styled.form `
+//     display: flex;
+//     flex-direction: column;
+//     justify-content: center;
+//     align-items: center;
+//     margin-top: 40px;
+// `;
 
-const EnterInput = styled.input `
-    margin-top: 20px;
-    margin-bottom: 20px;
-`;
+// const EnterInput = styled.input `
+//     margin-top: 20px;
+//     margin-bottom: 20px;
+// `;
 
-const LoginButton = styled.button `
-    margin-top: 30px;
-`;
-// end styled-components
+// const LoginButton = styled.button `
+//     margin-top: 30px;
+// `;
+// // end styled-components
 
-const Login = ({ history, userName, setUserName }) => {
+// const Login = ({ history, userName, setUserName }) => {
    
-    // const [userName, setUserName] = useState({
-    //     username: '',
-    //     password: ''
-    // });
+//     // const [userName, setUserName] = useState({
+//     //     username: '',
+//     //     password: ''
+//     // });
 
 
-    const { register, handleSubmit, errors } = useForm();
+//     const { register, handleSubmit, errors } = useForm();
 
-    const handleChange = event => {
-        //console.log('event', event.target.value);
+//     const handleChange = event => {
+//         //console.log('event', event.target.value);
 
-        setUserName({...userName, [event.target.name]: event.target.value });
-        //console.log('this is the handlechange username: ', {userName});
-    };
+//         setUserName({...userName, [event.target.name]: event.target.value });
+//         //console.log('this is the handlechange username: ', {userName});
+//     };
 
-    const submitForm = e => {
-        //event.preventDefault();
-        //history.push(`/WelcomePage/${userName.username}`);
-        // history.push('/WelcomePage');
-        // setUserName({user: '', password: '' });
-        // e.preventDefault();
-        axios
-          .post("https://weight-lifting1.herokuapp.com/api/auth/login", userName)
-          .then(res => {
-            localStorage.setItem("token", res.data.payload);
-            history.push("/WelcomePage");
-          })
-          .catch(err => console.error(err));
-    };
+//     const submitForm = e => {
+//         //event.preventDefault();
+//         //history.push(`/WelcomePage/${userName.username}`);
+//         // history.push('/WelcomePage');
+//         // setUserName({user: '', password: '' });
+//         // e.preventDefault();
+//         axios
+//           .post("https://weight-lifting1.herokuapp.com/api/auth/login", userName)
+//           .then(res => {
+//             localStorage.setItem("token", res.data.payload);
+//             history.push("/WelcomePage");
+//           })
+//           .catch(err => console.error(err));
+//     };
 
-    console.log('this is assigned userName object: ', {userName});
-    console.log('this is assigned username: ', userName.username);
-    console.log('this is assigned password: ', userName.password);
+//     console.log('this is assigned userName object: ', {userName});
+//     console.log('this is assigned username: ', userName.username);
+//     console.log('this is assigned password: ', userName.password);
     
-    return (
-        <div>
-            <nav>
-                <Link to='/newUser'>New User? Register Here</Link>
-            </nav>
-            <FormHeading>Current User Log In</FormHeading>
-            <FormSetup onSubmit={handleSubmit(submitForm)}>
-                <label htmlFor='username'>User Name</label>
-                <EnterInput
-                    id='username'
-                    type='text'
-                    name='username'
-                    placeholder='Minimum 5 Characters'
-                    onChange={handleChange}
-                    value={userName.username}
-                    ref={register({ required: true, minLength: 5 })}
-                />
-                {errors.username && errors.username.type === 'required' && (
-                    <p>This is required</p>
-                )}
-                {errors.username && errors.username.type === 'minLength' && (
-                    <p>Must be 5 characters in length</p>
-                )}
-                <label htmlFor='password'>Password</label>
-                <EnterInput
-                    id='password'
-                    type='password'
-                    name='password'
-                    placeholder='Minimum 5 Characters'
-                    onChange={handleChange}
-                    value={userName.password}
-                    ref={register({ required: true, minLength: 5 })}
-                />
-                {errors.password && errors.password.type === 'required' && (
-                    <p>This is required</p>
-                )}
-                {errors.password && errors.password.type === 'minLength' && (
-                    <p>Must be 5 characters in length</p>
-                )}
-                <LoginButton type='submit'>Log In</LoginButton>
-            </FormSetup>
-           {/*<WelcomePage id={userName.username} /> */}
-        </div>
-    );
-};
+//     return (
+//         <div>
+//             <nav>
+//                 <Link to='/newUser'>New User? Register Here</Link>
+//             </nav>
+//             <FormHeading>Current User Log In</FormHeading>
+//             <FormSetup onSubmit={handleSubmit(submitForm)}>
+//                 <label htmlFor='username'>User Name</label>
+//                 <EnterInput
+//                     id='username'
+//                     type='text'
+//                     name='username'
+//                     placeholder='Minimum 5 Characters'
+//                     onChange={handleChange}
+//                     value={userName.username}
+//                     ref={register({ required: true, minLength: 5 })}
+//                 />
+//                 {errors.username && errors.username.type === 'required' && (
+//                     <p>This is required</p>
+//                 )}
+//                 {errors.username && errors.username.type === 'minLength' && (
+//                     <p>Must be 5 characters in length</p>
+//                 )}
+//                 <label htmlFor='password'>Password</label>
+//                 <EnterInput
+//                     id='password'
+//                     type='password'
+//                     name='password'
+//                     placeholder='Minimum 5 Characters'
+//                     onChange={handleChange}
+//                     value={userName.password}
+//                     ref={register({ required: true, minLength: 5 })}
+//                 />
+//                 {errors.password && errors.password.type === 'required' && (
+//                     <p>This is required</p>
+//                 )}
+//                 {errors.password && errors.password.type === 'minLength' && (
+//                     <p>Must be 5 characters in length</p>
+//                 )}
+//                 <LoginButton type='submit'>Log In</LoginButton>
+//             </FormSetup>
+//            {/*<WelcomePage id={userName.username} /> */}
+//         </div>
+//     );
+// };
 
-export default Login;
+// export default Login;
--- muscle-pro/src/components/WelcomePage.js
+++ muscle-pro/src/components/WelcomePage.js
@@ -1,34 +1,34 @@
-import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
-import Exercises from './Exercises';
-import axios from 'axios';
-import styled from 'styled-components';
+// import React, { useState, useEffect } from 'react';
+// import { Link } from 'react-router-dom';
+// import Exercises from './Exercises';
+// import axios from 'axios';
+// import styled from 'styled-components';
 
-// styled-components
-const Card = styled.div `
-  border: solid black 2px;
-  margin: 50px 250px 50px 250px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  background-color: blue;
-`;
+// // styled-components
+// const Card = styled.div `
+//   border: solid black 2px;
+//   margin: 50px 250px 50px 250px;
+//   display: flex;
+//   justify-content: center;
+//   align-items: center;
+//   background-color: blue;
+// `;
 
-const Date = styled.h1 `
-  color: yellow;
-  font-weight: bold;
-`;
+// const Date = styled.h1 `
+//   color: yellow;
+//   font-weight: bold;
+// `;
 
-const Info = styled.ul `
-  list-style-type: none;
-`;
+// const Info = styled.ul `
+//   list-style-type: none;
+// `;
 
-const ListItems = styled.li `
-  color: white;
-`;
-// end styled-components
+// const ListItems = styled.li `
+//   color: white;
+// `;
+// // end styled-components
 
-const WelcomePage = ({ userName, setUserName, exercise, exercises, setExercises }) => {
+// const WelcomePage = ({ userName, setUserName, exercise, exercises, setExercises }) => {
     
     // set default exercise values to state
   //  const [exercises, setExercises] = useState([]);
@@ -52,46 +52,46 @@ const WelcomePage = ({ userName, setUserName, exercise, exercises, setExercises 
   };
   */
 
-  useEffect(() => {
-    axios.get(`https://weight-lifting1.herokuapp.com/api/user/all`)
-      .then(response => {
-        console.log(response);
-        setExercises(response);
-      })
-      .catch(error => {
-        console.log("Did not connect to API", error);
-      })
-  }, []);
+  // useEffect(() => {
+  //   axios.get(`https://weight-lifting1.herokuapp.com/api/user/all`)
+  //     .then(response => {
+  //       console.log(response);
+  //       setExercises(response);
+  //     })
+  //     .catch(error => {
+  //       console.log("Did not connect to API", error);
+  //     })
+  // }, []);
 
-    return (
-        <div>
-            <header>
-                <h1>Muscle Pro</h1>
-                <h2>Weight Lifting Journal</h2>
-            </header>
-            <nav>
-                <Link to='/Exercises'>Add New Exercises</Link><br />
-                <Link to='/'>Logout</Link>
-            </nav>
-          {/*  <Exercises addNewExercise={addNewExercise} /> */}
-            {/* exercises.map over data here and render to UI */}
-            {exercises.map(exercise => {
-                return (
-                    <Card>
-                        <Date>Date: {exercise.date}</Date>
-                        <Info>
-                            <ListItems>Muscle Group: {exercise.muscle_group}</ListItems>
-                            <ListItems>Name of Exercise: {exercise.exercise_name}</ListItems>
-                            <ListItems>Weight Lifted: {exercise.weight_number}</ListItems>
-                            <ListItems>Sets: {exercise.sets}</ListItems>
-                            <ListItems>Reps: {exercise.reps}</ListItems>
-                            <ListItems>Goals: {exercise.goals}</ListItems>
-                        </Info>
-                    </Card>
-                )
-            })}
-        </div>
-    );
-};
+//     return (
+//         <div>
+//             <header>
+//                 <h1>Muscle Pro</h1>
+//                 <h2>Weight Lifting Journal</h2>
+//             </header>
+//             <nav>
+//                 <Link to='/Exercises'>Add New Exercises</Link><br />
+//                 <Link to='/'>Logout</Link>
+//             </nav>
+//           {/*  <Exercises addNewExercise={addNewExercise} /> */}
+//             {/* exercises.map over data here and render to UI */}
+//             {exercises.map(exercise => {
+//                 return (
+//                     <Card>
+//                         <Date>Date: {exercise.date}</Date>
+//                         <Info>
+//                             <ListItems>Muscle Group: {exercise.muscle_group}</ListItems>
+//                             <ListItems>Name of Exercise: {exercise.exercise_name}</ListItems>
+//                             <ListItems>Weight Lifted: {exercise.weight_number}</ListItems>
+//                             <ListItems>Sets: {exercise.sets}</ListItems>
+//                             <ListItems>Reps: {exercise.reps}</ListItems>
+//                             <ListItems>Goals: {exercise.goals}</ListItems>
+//                         </Info>
+//                     </Card>
+//                 )
+//             })}
+//         </div>
+//     );
+// };
 
-export  default WelcomePage;
+// export  default WelcomePage;
